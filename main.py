f76d63035a5975dfc64e25c3fbafc744855ae1a7
Viacheslav <ixi.timoshenko.ixi@gmail.com>
2026-01-02 22:48:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 from expression_module import calculate_expression
 
-def main():
-    try:
-        n = int(input("Введіть натуральне число n: "))
-        value = calculate_expression(n)
-        print(f"Значення виразу = {value:.6f}")
-    except ValueError as e:
-        print("Помилка:", e)
-
-if __name__ == "__main__":
-    main()
+n = int(input("Введіть натуральне число n: "))
+print(calculate_expression(n))
